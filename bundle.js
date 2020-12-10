85d59e8ba39ddfb265cf74b583612f116bcaea49
wadhwa1 <wadhwa1@science.regn.net>
2020-12-10 12:35:52 +0530

    Update app, add bundle.js

---
+++ bundle.js
@@ -0,0 +1,4 @@
+window.renderApp1 = function (elementId) {
+    var element = document.getElementById(elementId);
+    element.innerHTML = "<h2>Hello world</h2><input type='button' value='Submit and alert' onclick='myalert()'/><script>window.myalert = function() {alert('Hello this is my alert')}</script>";
+}
